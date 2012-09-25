c8d1d39e9ba11d14b7bb9eeb7045d9f5426c2698
Michiel Sikkes <michiel@firmhouse.com>
2012-09-25 20:32:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apphakker::Application.config.secret_token = 'efb8403b8a8994395a6e245650e3b3ccc86214b2ee2b3d9c1e6b2d48df891a18d87e90e68a3b8925a66ccc8693ee293c9c530bad56acc239be3bf0af9390f5ae'
