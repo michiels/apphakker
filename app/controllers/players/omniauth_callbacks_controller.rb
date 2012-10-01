488c0427ad67c9595f41e1725b134a0cf8032b36
Michiel Sikkes <michiel@firmhouse.com>
2012-10-01 08:31:02 +0200

    Added callback for GitHub omniauth

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -1,7 +1,14 @@
 class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
-    
+    logger.info request.env['omniauth.auth']
+    # player = Player.find_for_github_oauth(request.env['omniauth.auth'], current_player)
+
+    # if user.persisted?
+    #   sign_in_and_redirect user, :event => :authentication
+    # else
+    #   raise request.env['omniauth.auth']
+    # end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Apphakker::Application.routes.draw do
   devise_for :players, :controllers => { :omniauth_callbacks => "players/omniauth_callbacks"}
 
+  devise_scope :player do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_player_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_session
+  end
+
   root :to => "home#index"
 end
