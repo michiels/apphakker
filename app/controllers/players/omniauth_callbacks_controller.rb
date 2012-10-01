d8b9c41e7e92ce1e9b787d42df5ea6a3e9d4d304
Michiel Sikkes <michiel@firmhouse.com>
2012-10-01 21:41:37 +0200

    Updated validating of email

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -4,8 +4,8 @@ class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth_info = request.env['omniauth.auth']
 
     player = Player.find_or_initialize_by_provider_and_uid(auth_info.provider, auth_info.uid)
-    player.name = auth_info.info.name
-    player.email ||= auth_info.info.email
+    player.name = auth_info.extra.raw_info.name
+    player.email ||= auth_info.extra.raw_info.email
 
     if player.save
       sign_in_and_redirect :player, player, :event => :authentication
