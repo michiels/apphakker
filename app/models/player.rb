37f78cc6941d42fb50235ea419ebf5d5f78db503
Michiel Sikkes <michiel@firmhouse.com>
2012-10-01 21:35:43 +0200

    Disable confirmable for now

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable, :recoverable, :validatable, :database_authenticatable,
-  devise :omniauthable, :registerable, :confirmable # :rememberable, :trackable,
+  devise :omniauthable, :registerable #,  :confirmable # :rememberable, :trackable,
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
