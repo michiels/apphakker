36fd421d423b08b01bb76a1f8b9061cf35fe5175
Michiel Sikkes <michiel.sikkes@gmail.com>
2012-12-17 22:58:00 +0100

    Test for claiming an assignment

--- test/functional/achievements_controller_test.rb
+++ test/functional/achievements_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AchievementsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "claim should create a new achievement for a user" do
+    user = players(:michiel)
+    sign_in players(:michiel)
+
+    assert_difference "user.achievements.count" do
+      get :claim, :id => assignments(:github), :format => :js
+    end
+
+    assert_response :success
+  end
+
 end
