4c4a575d124738dcc2ad0de1a8433b79b13d515f
Michiel Sikkes <michiel.sikkes@gmail.com>
2012-12-18 21:50:32 +0100

    Refactor into Dashboard layout and added an AssignmentSet show view

--- app/controllers/assignment_sets_controller.rb
+++ app/controllers/assignment_sets_controller.rb
@@ -1,8 +1,9 @@
 class AssignmentSetsController < ApplicationController
+  layout "dashboard"
   before_filter :authenticate_player!
 
   def show
-    
+    @assignment_set = AssignmentSet.find(params[:id])
   end
 
 end
